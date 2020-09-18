dd9fed7a7249ae952ca9a1c7d8adf254b0b4c7e1
Hajiman-ssi <kdj9248@naver.com>
2020-09-18 16:35:23 +0900

    Compatibility for the lastest version of Qwilight

--- src/skin_processor.py
+++ src/skin_processor.py
@@ -7,16 +7,15 @@ def dictobj(name, value):
             "value": value
             }
 
-def ui_draw(ver, name):
-    if v.isnewer(ver, "1.3.0"):
-
 
 class Skin:
+    # Reference: https://taehui.ddns.net/bbs/board.php?bo_table=rtfm&wr_id=3
+    #            (Korean)
 
     def __init__(self):
 
+        # Initialize
         self.name = ""
-        self.client_ver = v.ver("0.1")
         self.resource = ""
         self.luafile = ""
 
@@ -26,17 +25,20 @@ class Skin:
         self.render_order = []
         self.style = []
         self.drawing = []
+        self.input_mode = []
 
     def load(self, yamlfile):
+
+        # Check type
         if not isinstance(yamlfile, yl.YAMLFile):
             raise TypeError
 
+        # Load header
         self.name = yamlfile.file_name
-        self.client_ver = v.ver(yamlfile.header["date"])
         self.resource = yamlfile.header["zip"]
         self.luafile = yamlfile.header["lua"]
 
-        # Load Number of frames and Framerates
+        # Load numbers of frames and framerates
         for value_name in yamlfile.frame:
             obj_name = value_name.split('-')[0]
             if value_name.endswith("framerate"):
@@ -46,27 +48,41 @@ class Skin:
                 self.frame.append(dictobj(obj_name,
                                           yamlfile.frame[value_name]))
 
+        # Load result rank color
         for value_name in yamlfile.paint:
-            if value_name != "default-faint":
-                self.color.append(dictobj(value_name.replace('-', '_'),
-                                          yamlfile.paint[value_name]))
-            else:
-                self.color.append(dictobj("legacy_"
-                                          + value_name.replace('-', '_'),
+            if value_name.startswith("quit-"):
+                ranks = ('x', 'u', 's', 'a', 'b', 'c', 'd')
+                rank_number = int(value_name.split('-')[-1])
+
+                self.color.append(dictobj("rank_%s" % ranks[rank_number],
                                           yamlfile.paint[value_name]))
 
+            else:
+                self.color.append(dictobj(value_name.replace('-', '_'),
+                                          yamlfile.paint[value_name]))
+
+        # Load Styles(function)
         for value_name in yamlfile.function:
+            # Render order(pipeline)
             if value_name == "pipeline":
                 pipeline_str = str(yamlfile.function["pipeline"])
                 for obj_num in pipeline_str.split(","):
                     self.render_order.append(int(obj_num))
                 del pipeline_str
 
+            # Key style number configure
             elif value_name.startswith("ui-drawing-"):
                 img_num = int(value_name.split('-')[-1])
                 input_num = str(yamlfile.function[value_name]).split(",")
                 self.drawing.append({"num": img_num,
                                      "target": input_num})
+
+            # Order key style by each Key modes
+            elif value_name.startswith("ui-input-mode-"):
+                mode_num = int(value_name.split('-')[-1])
+                order_num = str(yamlfile.function[value_name]).split(",")
+                self.input_mode.append({"mode": mode_num,
+                                        "target": order_num})
 
 
 # For test
@@ -78,7 +94,6 @@ if __name__ == '__main__':
 
     # Debug Data
     print("Name:", example_skin.name)
-    print("Client Version for:", example_skin.client_ver)
     print("Resouse Zip:", example_skin.resource)
     print("Lua Script:", example_skin.luafile)
     print("")
@@ -96,5 +111,9 @@ if __name__ == '__main__':
     print("")
     print("Style")
     print(example_skin.style)
+    print("")
     print("Drawing")
+    print(example_skin.drawing)
+    print("")
+    print("Key Sets")
     print(example_skin.drawing)
