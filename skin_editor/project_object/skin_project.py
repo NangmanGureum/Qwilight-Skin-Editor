d8d0cf25a3679c5987c4f9725e22fd5dcf5d9c1b
NangmanGureum <lastkwak@gmail.com>
2021-08-11 20:21:05 +0900

    format code

--- skin_editor/project_object/skin_project.py
+++ skin_editor/project_object/skin_project.py
@@ -2,7 +2,7 @@ class Skin():
 
     def __init__(self) -> None:
         # Refer to format.qwsk
-        # .qwer stands for QWilight SKin
+        # .qwsk stands for QWilight SKin
 
         # Skin metadata
         self.title = str()
@@ -38,6 +38,32 @@ class Skin():
         self.frame = list()
         """
         OBJECT NAME LIST
-
         
+        - note
+        - judgement
+        - line
+        - button
+        - explose
+        - comber_number
+        - in_play_result
         """
+        self.style = {
+            "pipeline": [
+                49, 1, 26, 3, 4, 7, 50, 8, 31, 102, 103, 100, 101, 2, 6, 9, 10, 11, 12, 13, 14, 30, 15, 16, 27, 28, 29, 33, 43, 44, 46, 47, 48
+            ],
+            "note": {
+                "logn_note_edge": False,
+
+            },
+            "lines": {
+                "modes": [
+                    {
+                        "mode": "1",
+                        "line_num": []
+                    },
+                ]
+            },
+            "judge": {
+                "setable_judgment_position": False,
+            },
+        }
